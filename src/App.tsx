e21106263313d3f2e229f5b9892eec638dbaa80c
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-09-12 14:36:16 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import computerImageUrl from './assets/computer.png'
 
 function App() {
   return (
-    <main className="mx-auto my-6 max-w-2xl">
-      <h1 className="mb-4 text-2xl font-bold">Let's write</h1>
-      <img
-        src={computerImageUrl}
-        alt="Computer"
-        style={{ maxWidth: '400px' }}
-      />
-    </main>
+    <>
+      <header className="bg-green-600 p-3 sticky top-0 left-0 right-0 w-full">
+        <h1>This is the header</h1>
+      </header>
+
+      <main className="mx-auto max-w-2xl">
+        {new Array(10).fill('x').map((_, i) => (
+          <img
+            src={computerImageUrl}
+            alt="Computer"
+            className="mb-4"
+            style={{ maxWidth: '400px' }}
+          />
+        ))}
+      </main>
+    </>
   )
 }
 
