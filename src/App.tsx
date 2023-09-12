b1e83b0a5993022c33a2d85e4fb17c4a71b1e34a
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-09-12 14:36:58 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import computerImageUrl from './assets/computer.png'
 function App() {
   return (
     <>
-      <header className="bg-green-600 p-3 sticky top-0 left-0 right-0 w-full">
-        <h1>This is the header</h1>
+      <header className="sticky left-0 right-0 top-0 w-full bg-green-600 p-3">
+        <h1 className="text-center">
+          This is a sticky header. Scroll and it will stick!
+        </h1>
       </header>
 
       <main className="mx-auto max-w-2xl">
